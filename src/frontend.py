a832a0bd27073a2f672177ae11ba60789c894316
jayanra <yanzonjavier@gmail.com>
2023-11-11 21:14:33 +0100

    Se actualiza frontend y plot

---
+++ src/frontend.py
@@ -0,0 +1,226 @@
+import zipfile 
+from datetime import datetime, timedelta
+
+import requests
+import numpy as np
+import pandas as pd
+import streamlit as st
+import geopandas as gpd
+import pydeck as pdk
+import numpy as np
+
+from src.inference import (
+    load_predictions_from_store,
+    load_batch_of_features_from_store
+)
+from src.paths import DATA_DIR
+from src.plot import plot_one_sample
+
+st.set_page_config(layout="wide")
+
+# title
+# current_date = datetime.strptime('2023-01-05 12:00:00', '%Y-%m-%d %H:%M:%S')
+current_date = pd.to_datetime(datetime.utcnow(), utc=True).floor('H')
+st.title(f'Bike demand prediction 🚲')
+st.header(f'{current_date} UTC')
+
+progress_bar = st.sidebar.header('⚙️ Working Progress')
+progress_bar = st.sidebar.progress(0)
+N_STEPS = 6
+
+def load_shape_data_file() -> gpd.geodataframe.GeoDataFrame:
+    """
+    Fetches remote file with shape data, that we later use to plot the
+    different pickup_location_ids on the map of NYC.
+
+    Raises:
+        Exception: when we cannot connect to the external server where
+        the file is.
+
+    Returns:
+        GeoDataFrame: columns -> (OBJECTID	Shape_Leng	Shape_Area	zone	LocationID	borough	geometry)
+    """
+    # download zip file
+    URL = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/transporte-y-obras-publicas/estaciones-bicicletas-publicas/estaciones-de-bicicletas-zip.zip'
+    response = requests.get(URL)
+    path = DATA_DIR / f'IE-Estaciones.zip'
+    if response.status_code == 200:
+        open(path, "wb").write(response.content)
+    else:
+        raise Exception(f'{URL} is not available')
+
+    # unzip file
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        zip_ref.extractall(DATA_DIR / 'IE-Estaciones')
+
+    # load and return shape file
+    return gpd.read_file(DATA_DIR / 'IE-Estaciones/IE-Estaciones.shp').to_crs('epsg:4326') # 3857
+
+@st.cache_data
+def _load_batch_of_features_from_store(current_date: datetime) -> pd.DataFrame:
+    """Wrapped version of src.inference.load_batch_of_features_from_store, so
+    we can add Streamlit caching
+
+    Args:
+        current_date (datetime): _description_
+
+    Returns:
+        pd.DataFrame: n_features + 2 columns:
+            - `rides_previous_N_hour`
+            - `rides_previous_{N-1}_hour`
+            - ...
+            - `rides_previous_1_hour`
+            - `pickup_hour`
+            - `pickup_location_id`
+    """
+    return load_batch_of_features_from_store(current_date)
+
+@st.cache_data
+def _load_predictions_from_store(
+    from_pickup_hour: datetime,
+    to_pickup_hour: datetime
+    ) -> pd.DataFrame:
+    """
+    Wrapped version of src.inference.load_predictions_from_store, so we
+    can add Streamlit caching
+
+    Args:
+        from_pickup_hour (datetime): min datetime (rounded hour) for which we want to get
+        predictions
+
+        to_pickup_hour (datetime): max datetime (rounded hour) for which we want to get
+        predictions
+
+    Returns:
+        pd.DataFrame: 2 columns: pickup_location_id, predicted_demand
+    """
+    return load_predictions_from_store(from_pickup_hour, to_pickup_hour)
+
+with st.spinner(text="Downloading shape file to plot bike stations"):
+    geo_df = load_shape_data_file()
+    st.sidebar.write('✅ Shape file was downloaded ')
+    progress_bar.progress(1/N_STEPS)
+
+with st.spinner(text="Fetching model predictions from the store"):
+    predictions_df = _load_predictions_from_store(
+        from_pickup_hour=current_date - timedelta(hours=1),
+        to_pickup_hour=current_date
+        )
+    predictions_df = predictions_df.reset_index(drop=True)
+    st.sidebar.write('✅ Model predictions arrived')
+    progress_bar.progress(2/N_STEPS)
+
+# Here we are checking the predictions for the current hour have already been computed
+# and are available
+next_hour_predictions_ready = \
+    False if predictions_df[predictions_df.pickup_hour == current_date].empty else True
+prev_hour_predictions_ready = \
+    False if predictions_df[predictions_df.pickup_hour == (current_date - timedelta(hours=1))].empty else True
+
+if next_hour_predictions_ready:
+    # predictions for the current hour are available
+    predictions_df = predictions_df[predictions_df.pickup_hour == current_date]
+elif prev_hour_predictions_ready:
+    # predictions for current hour are not available, so we use previous hour predictions
+    predictions_df = predictions_df[predictions_df.pickup_hour == (current_date - timedelta(hours=1))]
+    current_date = current_date - timedelta(hours=1)
+    st.subheader('⚠️ The most recent data is not yet available. Using last hour predictions')
+else:
+    raise Exception('Features are not available for the last 2 hours. Is your feature \
+                    pipeline up and running? 🤔')
+
+
+with st.spinner(text="Preparing data to plot"):
+
+    def pseudocolor(val, minval, maxval, startcolor, stopcolor):
+        """
+        Convert value in the range minval...maxval to a color in the range
+        startcolor to stopcolor. The colors passed and the the one returned are
+        composed of a sequence of N component values.
+
+        Credits to https://stackoverflow.com/a/10907855
+        """
+        f = float(val-minval) / (maxval-minval)
+        return tuple(f*(b-a)+a for (a, b) in zip(startcolor, stopcolor))
+    
+    df = pd.merge(geo_df, predictions_df,
+                  right_on='pickup_location_id',
+                  left_on='ID',
+                  how='inner')
+    
+    BLACK, ORANGE = (0, 0, 0), (255, 128, 0)
+    selected_columns = [c for c in df.columns if c.startswith('rides_next_')]
+    df['max_hour'] = df[selected_columns].idxmax(axis=1)
+    df['color_scaling'] = df[selected_columns].max(axis=1) 
+    max_pred, min_pred = df['color_scaling'].max(), df['color_scaling'].min()
+    df['fill_color'] = df['color_scaling'].apply(lambda x: pseudocolor(x, min_pred, max_pred, BLACK, ORANGE))
+    
+    progress_bar.progress(3/N_STEPS)
+
+with st.spinner(text="Generating BsAs Map"):
+
+    INITIAL_VIEW_STATE = pdk.ViewState(
+        latitude=-34.60280869220721,
+        longitude=-58.42827362585887,
+        zoom=11,
+        max_zoom=16,
+        pitch=45,
+        bearing=0
+    )
+    layer = pdk.Layer("ColumnLayer",
+                          data=df,
+                          get_position=["Lon", "Lat"],
+                          get_elevation=['color_scaling'],
+                          auto_highlight=True,
+                          radius=50,
+                          elevation_scale=300,                          
+                          get_fill_color="fill_color",
+                          get_line_color=[255, 255, 255],
+                          pickable=True,
+                          extruded=True,
+                          coverage=1)
+    
+
+    tooltip = {"html": "<b>Zone:</b> [{ID} - {DIRECCION}] <br /> <b>Max: </b> {color_scaling} rides - {max_hour}"}
+
+    r = pdk.Deck(
+        layers=[layer],
+        initial_view_state=INITIAL_VIEW_STATE,
+        tooltip=tooltip
+    )
+
+    st.pydeck_chart(r)
+    progress_bar.progress(4/N_STEPS)
+
+with st.spinner(text="Fetching batch of features used in the last run"):
+    features_df = _load_batch_of_features_from_store(current_date)
+    features_df=features_df.reset_index(drop=True)
+    st.sidebar.write('✅ Inference features fetched from the store')
+    progress_bar.progress(5/N_STEPS)
+
+with st.spinner(text="Plotting time-series data"):
+    
+    predictions_df = np.clip(predictions_df[selected_columns], 0, None) #Hago esto para limitar los valores a cero y que no de ninguno negativo 
+    
+
+    predictions_df['max'] = predictions_df[selected_columns].max(axis=1) 
+    sorted_indices = predictions_df['max'].sort_values(ascending=False).index
+    predictions_df = predictions_df.drop('max', axis=1)
+
+    # Selecciona las 10 filas principales 
+    top_10_indices = sorted_indices[:10]
+
+
+    # plot each time-series with the prediction 
+    for row_id in top_10_indices:
+        #if row_id < len(predictions_df):
+            fig = plot_one_sample(
+                example_id=row_id,
+                features=features_df,
+                targets=predictions_df,
+                predictions=predictions_df,
+                directions=geo_df[['ID', 'DIRECCION']]
+            )
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True, width=1000)
+
+    progress_bar.progress(6/N_STEPS)
--- src/plot.py
+++ src/plot.py
@@ -6,75 +6,82 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 def plot_one_sample(
-    features: pd.DataFrame,
-    targets: pd.DataFrame, #pd.Series, 
     example_id: int,
+    features: pd.DataFrame,
+    targets: Optional[pd.DataFrame] = None, #pd.Series, 
     predictions: Optional[pd.DataFrame] = None,
+    directions: Optional[pd.DataFrame] = None
 ):
     """"""
-    features_ = features.iloc[example_id]
-    target_ = targets.iloc[example_id]
+    if directions is not None:
+        features_ = pd.merge(features, directions, left_on='pickup_location_id', right_on='ID', how='left')
+        features_ = features_.iloc[example_id] #Lo modifique porque en otros casos no filtrando^
+    else:
+        features_ = features.iloc[example_id] #Lo modifique porque en otros casos no filtrando^ 
     
-    # ts_columns = [c for c in features.columns if c.startswith('rides_previous_')]
-    # ts_values = [features_[c] for c in ts_columns] + [target_]
-    ts_columns_features = [c for c in features.columns if c.startswith('rides_previous_')]
-    ts_columns_targets = [c for c in targets.columns if c.startswith('rides_next_')]
-    ts_values_features = [features_[c] for c in ts_columns_features] 
-    ts_values_targets = [target_[c] for c in ts_columns_targets] 
-    # ts_dates = pd.date_range(
-    #     features_['pickup_hour'] - timedelta(hours=len(ts_columns)),
-    #     features_['pickup_hour'],
-    #     freq='H'
-    # )
-    ts_dates_features = pd.date_range(
-        features_['pickup_hour'] - timedelta(hours=len(ts_columns_features)),
-        features_['pickup_hour'] - timedelta(hours=1),
-        freq='H'
-    )
-    ts_dates_targets = pd.date_range(
+    
+    if targets is not None:
+        target_ = targets.iloc[example_id] #Lo modifique porque en otros casos no filtrando
+        ts_columns_targets = [c for c in targets.columns if c.startswith('rides_next_')]
+        ts_values_targets = [target_[c] for c in ts_columns_targets] 
+        ts_dates_targets = pd.date_range(
         features_['pickup_hour'],
         features_['pickup_hour'] + timedelta(hours=len(ts_columns_targets)-1),
         freq='H'
     )
+    else:
+        target_ = None
     
-    # line plot with past values
-    # title = f'Pick up hour={features_["pickup_hour"]}, location_id={features_["pickup_location_id"]}'
-    # fig = px.line(
-    #     x=ts_dates, y=ts_values,
-    #     template='plotly_dark',
-    #     markers=True, title=title
-    # )
+    # features_ = features[features['pickup_location_id'] == example_id]
+    # target_ = targets[targets['pickup_location_id'] == example_id]
+
+   
+
+    ts_columns_features = [c for c in features.columns if c.startswith('rides_previous_')]
+    
+    ts_values_features = [features_[c] for c in ts_columns_features] 
+    
+
+    ts_dates_features = pd.date_range(
+        features_['pickup_hour'] - timedelta(hours=len(ts_columns_features)), #Agregp .iloc[0] `porque quiero tomar el unico valor de esa serie`
+        features_['pickup_hour'] - timedelta(hours=1),
+        freq='H'
+    )
+
+
     fig = go.Figure()
-    title = f'Pick up hour={features_["pickup_hour"]}, location_id={features_["pickup_location_id"]}'
+    if directions is not None:
+        title = f'Pick up hour= {features_["pickup_hour"]}, location_id= {features_["pickup_location_id"]}, direction= {features_["DIRECCION"]}'
+    else:
+        title = f'Pick up hour= {features_["pickup_hour"]}, location_id= {features_["pickup_location_id"]}'
     fig = px.line( x=ts_dates_features, y=ts_values_features,
                 template='plotly_dark',
                 markers=True, title=title)
-    #features_fig.update_traces(line_color='blue')
-    #fig.add_traces(features_fig.data)
+
+    if targets is not None:
+        targets_fig = px.line(x=ts_dates_targets, y=ts_values_targets,  
+                    template='plotly_dark', 
+                    markers=True, title='actual values')
+        targets_fig.update_traces(line_color='green')
+        fig.add_traces(targets_fig.data)
     
-    # green line for the values we wanna predict
-    # fig.add_scatter(x=ts_dates[-1:], y=[target_],
-    #                 line_color='green',
-    #                 mode='markers', marker_size=10, name='actual value') 
-    targets_fig = px.line(x=ts_dates_targets, y=target_.values.tolist(),
-                template='plotly_dark', 
-                markers=True, title='actual values')
-    targets_fig.update_traces(line_color='green')
-    fig.add_traces(targets_fig.data)
-    #fig.show()
 
     if predictions is not None:
-        # red line for the predicted values, if passed
-        # prediction_ = predictions.iloc[example_id]
-        # fig.add_scatter(x=ts_dates[-1:], y=[prediction_],
-        #                 line_color='red',
-        #                 mode='markers', marker_symbol='x', marker_size=15,
-        #                 name='prediction')  
+ 
         prediction_ = predictions.iloc[example_id]
-        prediction_fig = px.line(x=ts_dates_targets, y=prediction_.values.tolist(),
+        #prediction_ = predictions[predictions['pickup_location_id'] == example_id]
+        ts_columns_predictions = [c for c in predictions.columns if c.startswith('rides_next_')]       
+        ts_values_predictions = [prediction_[c] for c in ts_columns_predictions]
+        ts_dates_predictions = pd.date_range(
+        features_['pickup_hour'],
+        features_['pickup_hour'] + timedelta(hours=len(ts_columns_predictions)-1),
+        freq='H'
+        )
+ 
+        prediction_fig = px.line(x=ts_dates_predictions, y=ts_values_predictions, 
                 template='plotly_dark', 
                 markers=True, title='predicted values')
-        prediction_fig.update_traces(line_color='red')
+        prediction_fig.update_traces(line_color='darkorange')
         fig.add_traces(prediction_fig.data)   
 
     return fig
